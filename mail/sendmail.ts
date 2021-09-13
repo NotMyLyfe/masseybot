d9bc7f5b474920f44f2f3623b6f186542ee7074e
Gordon Lin <contact@gordonlin.ca>
2021-09-13 17:45:29 -0400

    Changed axios method

--- mail/sendmail.ts
+++ mail/sendmail.ts
@@ -11,17 +11,18 @@ export default function (email : string, token : string, name : string){
             text: `Hello ${name}!\n\nThank you for joining a club here at Vincent Massey Secondary School on Discord.\n\nIn order to maintain member privacy and security, we are requiring all users to verify themselves with their student email. To complete your account verification, please visit ${link} .\n\nIf you are in need of any assistance, please contact a Discord administrator. We look forward to another year at Vincent Massey!`,
             html: `Hello ${name}!<br><br>Thank you for joining a club here at Vincent Massey Secondary School on Discord.<br><br>In order to maintain member privacy and security, we are requiring all users to verify themselves with their student email. To complete your account verification, please <a href="${link}">click here</a>.<br><br>If you are in need of any assistance, please contact a Discord administrator. We look forward to another year at Vincent Massey!`
         });
-        axios.post(
-            `https://api.mailgun.net/v3/${process.env.DOMAIN}/messages`, msg, {
-                auth: {
-                    username: "api",
-                    password: process.env.MAILGUN_APIKEY
-                },
-                headers:{
-                    'Content-Type':'application/x-www-form-urlencoded'
-                }
+        axios({
+            method: 'post',
+            url: `https://api.mailgun.net/v3/${process.env.DOMAIN}/messages`, 
+            data: msg,
+            auth: {
+                username: "api",
+                password: process.env.MAILGUN_APIKEY
+            },
+            headers:{
+                'Content-Type':'application/x-www-form-urlencoded'
             }
-        ).then(() => {
+        }).then(() => {
             return resolve(undefined);
         }).catch((err) => {
             console.log(err);
