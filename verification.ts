99f37a10d8a739a08aff31187536212fda46d0ba
Gordon Lin <contact@gordonlin.ca>
2021-09-12 21:09:02 -0400

    Added reCaptcha to verification

--- verification.ts
+++ verification.ts
@@ -2,7 +2,9 @@ require('dotenv').config();
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import { discordUsers } from './models/schema';
+import { RecaptchaV2 } from 'express-recaptcha/dist';
 
+const recaptcha = new RecaptchaV2(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY);
 const app = express();
 
 export interface TokenInterface{
@@ -11,29 +13,51 @@ export interface TokenInterface{
     name : string;
 }
 
-app.get('/verify/:id', async(req, res, next) => {
-    try{
-        let decoded = jwt.verify(req.params.id, process.env.JWT_SECRET) as TokenInterface;
-        let discordId = decoded.discordId;
-        let studentNumber = decoded.studentNumber;
-        let fullname = decoded.name;
-        await discordUsers.findOneAndUpdate({discordId : discordId}, 
-            {   
-                name : fullname,
-                discordId: discordId,
-                email: `${studentNumber}@student.publicboard.ca`
-            },
-            { new : true, upsert: true});
-        res.status(200).send("Alright! You have been verified!");
-    }
-    catch(err){
-        return next(err);
-    }
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+app.get('/verify/:id', recaptcha.middleware.render, (req, res) => {
+    res.render('index', { captcha : res.recaptcha });
 });
 
-app.use(function(err, req, res, next) {
-    console.log(err);
-    res.status(500).send('Token is invalid');
+app.post('/verify/:id', async(req, res) => {
+    recaptcha.verify(req, async(err, data) => {
+        if(!err){
+            try{
+                let decoded = jwt.verify(req.params.id, process.env.JWT_SECRET) as TokenInterface;
+                let discordId = decoded.discordId;
+                let studentNumber = decoded.studentNumber;
+                let fullname = decoded.name;
+                await discordUsers.findOneAndUpdate({discordId : discordId}, 
+                    {   
+                        name : fullname,
+                        discordId: discordId,
+                        email: `${studentNumber}@student.publicboard.ca`
+                    },
+                    { new : true, upsert: true});
+                    res.render('index', {
+                        data: "Alright! You have been verified!"
+                    });
+            }
+            catch(err){
+                res.render('index', {
+                    error: "Token invalid, contact an admin"
+                });
+            }
+        }
+        else{
+            res.render('index', {
+                error: "Captcha invalid, please try again",
+                path: req.path
+            });
+        }
+    });
+    
 });
 
 app.listen(3000, ()=>{
