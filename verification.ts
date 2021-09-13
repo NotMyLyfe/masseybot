460860764b847a0269eb0e332db43542355b1a9a
Gordon Lin <contact@gordonlin.ca>
2021-09-13 17:39:40 -0400

    Replaced reCaptcha with hCaptcha

--- verification.ts
+++ verification.ts
@@ -2,6 +2,8 @@ require('dotenv').config();
 import express, { request } from 'express';
 import jwt from 'jsonwebtoken';
 import axios from 'axios';
+import exphbs from 'express-handlebars';
+import qs from 'qs';
 import { discordUsers } from './models/schema';
 
 const app = express();
@@ -17,17 +19,36 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-app.set('views', __dirname + '/views');
+app.engine('handlebars', exphbs({
+    extname: "hbs",
+    defaultLayout: "",
+    layoutsDir: ""
+}));
+app.set('view engine', 'handlebars');
 
 app.get('/verify/:id', (req, res) => {
-    res.render(__dirname + '/views/verify.html');
+    res.render('verify', { captcha : process.env.HCAPTCHA_SITE_KEY});
 });
 
 app.post('/verify/:id', async(req, res) => {
-    console.log(req.body);
-    res.render(__dirname + '/views/verify.html');
-    /*recaptcha.verify(req, async(err, data) => {
-        if(!err){
+    if(!req.body['h-captcha-response']){
+        return res.render('verify', {
+            captcha : process.env.HCAPTCHA_SITE_KEY,
+            error : "Invalid captcha, please try again"
+        });
+    }
+
+    axios({
+        method: 'POST',
+        url: `https://hcaptcha.com/siteverify`,
+        data: qs.stringify({
+            response : req.body['h-captcha-response'],
+            secret : process.env.HCAPTCHA_SECRET_KEY
+        }),
+        headers: { 'Content-Type':'application/x-www-form-urlencoded' }
+    })
+    .then(async response => {
+        if(response.data.success){
             try{
                 let decoded = jwt.verify(req.params.id, process.env.JWT_SECRET) as TokenInterface;
                 let discordId = decoded.discordId;
@@ -40,24 +61,30 @@ app.post('/verify/:id', async(req, res) => {
                         email: `${studentNumber}@student.publicboard.ca`
                     },
                     { new : true, upsert: true});
-                    res.render('index', {
-                        data: "Alright! You have been verified!"
+                    res.render('verify', {
+                        success: true,
+                        redirect : process.env.REDIRECT_URL
                     });
             }
             catch(err){
-                res.render('index', {
-                    error: "Token invalid, contact an admin"
+                res.render('verify', {
+                    error: "Invalid token, contact an admin"
                 });
             }
         }
         else{
-            res.render('index', {
-                error: "Captcha invalid, please try again",
-                path: req.path
+            res.render('verify', {
+                captcha : process.env.HCAPTCHA_SITE_KEY,
+                error : "Captcha error, please try again"
             });
         }
+    })
+    .catch(err => {
+        res.render('verify', {
+            captcha : process.env.HCAPTCHA_SITE_KEY,
+            error : "Captcha error, please try again"
+        });
     });
-    */
 });
 
 app.listen(3000, ()=>{
