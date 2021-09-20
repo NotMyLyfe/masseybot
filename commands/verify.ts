52209a8655578795dae5b79f71cc2c225a743efd
Gordon Lin <contact@gordonlin.ca>
2021-09-20 19:25:00 -0400

    Added method to double check if user forgets full name

--- commands/verify.ts
+++ commands/verify.ts
@@ -10,7 +10,8 @@ module.exports = {
         .setName('verify')
         .setDescription("Verify yourself and associate yourself with your student ID")
         .addIntegerOption(option => option.setName('id').setDescription('Your student ID').setRequired(true))
-        .addStringOption(option => option.setName('name').setDescription('Your full name (with capitalization)').setRequired(true)),
+        .addStringOption(option => option.setName('name').setDescription('Your full name (with capitalization)').setRequired(true))
+        .addBooleanOption(option => option.setName('single').setDescription('Does your full name not include a last name?').setRequired(false)),
     async execute(interaction: CommandInteraction){
         const member = interaction.user;
         await interaction.deferReply({ ephemeral: interaction.inGuild() });
@@ -23,13 +24,18 @@ module.exports = {
             interaction.editReply("Invalid student ID, please enter a valid student ID");
             return;
         }
+        const name = interaction.options.getString('name');
+        if(!interaction.options.getBoolean('single') && name.split(' ').length < 2){
+            interaction.editReply("Did you forget to include both your first and last name? If your name doesn't include a last name, set the 'single' property to true");
+            return;
+        }
         const token = jwt.sign({
             discordId: member.id,
             studentNumber: idString,
-            name: interaction.options.getString('name')
+            name: name
         }, process.env.JWT_SECRET);
         const email = `${idString}@student.publicboard.ca`;
-        sendmail(email, token, interaction.options.getString('name')).then(async () => {
+        sendmail(email, token, name).then(async () => {
             await interaction.editReply(`We have sent an email to ${idString}@student.publicboard.ca. Please check your Microsoft365 email and click the link to complete verification. If you do not see it in your Inbox, **check your Junk Email folder** and report it as not junk!`)
         })
         .catch(async () => {
